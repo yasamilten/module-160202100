34e8f7da1b1ebde183fe956660bcf47d6fc14a9a
mervetafrali <160202100@kocaeli.edu.tr>
2019-12-06 20:23:36 +0300

    notes module created

---
+++ notes.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NotesRoutingModule } from './notes-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NotesRoutingModule
+  ]
+})
+export class NotesModule { }
